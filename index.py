8419fb3826c5c75c7771a0bcc2299d7724d2bc95
derlukaskk2 <lukas.dieckmann99@gmail.com>
2024-12-03 10:39:42 +0100

    changes to index.py

--- index.py
+++ index.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def test():
+    return "Hier gibt es nix zu sehen."
+
 @app.route('/flask', methods=['GET'])
 def index():
     return "Flask server"
